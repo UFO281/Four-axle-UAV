a76442b8bf4d1df23df892c5f599e166c518643e
gsh <geshuhan@orbbec.com>
2024-12-22 19:40:40 +0800

    [SPI_peripheral][add]{ add spi pripheral driver } [BUG ID]:{12345} [风险描述]:{}

--- Driver/Peripheral_Driver/SPI/SPI.h
+++ Driver/Peripheral_Driver/SPI/SPI.h
@@ -1,10 +1,20 @@
 /**
  * @file SPI.h
- * @author wls (ufo281@outlook.com) 
- * @brief 
+ * @author wls (ufo281@outlook.com)
+ * @brief
  * @version 1.0
  * @date 2024-11-18
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
+
+#ifndef __MYSPI_H
+#define __MYSPI_H
+
+void MySPI_Init(void);
+void MySPI_Start(void);
+void MySPI_Stop(void);
+uint8_t MySPI_SwapByte(uint8_t ByteSend);
+
+#endif
