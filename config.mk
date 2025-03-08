86c73ef4e750ceb7a0f4de23354da7e6fb293665
时空自由民 <ufo281@outlook.com>
2025-03-09 00:07:46 +0800

    实现app文件下的makefile编译文件

--- config.mk
+++ config.mk
@@ -1,32 +1,26 @@
 # 定义编译器
 CC = gcc
 
-PRO_DIR = /home/wls/git/four-axle-uav
-
-INC_DIRS =  APP 
-# Driver/Core \
-			# Driver/Device_Driver/AHT10 \
-			# Driver/Device_Driver/BMP280 \
-			# Driver/Device_Driver/LED \
-			# Driver/Device_Driver/MPU6050 \
-			# Driver/Device_Driver/MPU6050/eMPL \
-			# Driver/Peripheral_Driver/Delay \
-			# Driver/Peripheral_Driver/IIC \
-			# Driver/Peripheral_Driver/SPI \
-			# Driver/Peripheral_Driver/Uart \
-			# Driver/Peripheral_Driver_Lib/Include 
-
-INC_DIR = $(foreach dir, $(INC_DIRS), $(PRO_DIR) / @echo $(dir))
-
-
-I_INC_DIR = $(foreach dir, $(INC_DIRS), -I $(dir))
-
-
-
 # 项目路径
+PRO_DIR = /home/wls/stm32/four-axle-uav
 
 # 头文件包含目录
-INC_DIR = $(PRO_DIR)/Driver/inc
+INC_DIRS =  $(PRO_DIR)/APP \
+			$(PRO_DIR)/Driver/Core \
+			$(PRO_DIR)/Driver/Device_Driver/AHT10 \
+			$(PRO_DIR)/Driver/Device_Driver/BMP280 \
+			$(PRO_DIR)/Driver/Device_Driver/LED \
+			$(PRO_DIR)/Driver/Device_Driver/MPU6050 \
+			$(PRO_DIR)/Driver/Device_Driver/MPU6050/eMPL \
+			$(PRO_DIR)/Driver/Peripheral_Driver/Delay \
+			$(PRO_DIR)/Driver/Peripheral_Driver/IIC \
+			$(PRO_DIR)/Driver/Peripheral_Driver/SPI \
+			$(PRO_DIR)/Driver/Peripheral_Driver/Uart \
+			$(PRO_DIR)/Driver/Peripheral_Driver_Lib/Include 
+
+# -I 自动添加头文件路径
+I_INC_DIR = $(foreach dir, $(INC_DIRS), -I $(dir))
+
 
 # 编译生成目标文件目录
 BUILD_DIR = $(PRO_DIR)/build
@@ -34,6 +28,7 @@ BUILD_DIR = $(PRO_DIR)/build
 # 定义编译选项
 CFLAGS = -Wall -Wextra -O2 $(I_INC_DIR)
 
+# 定义生成的可执行文件目录&& bin hex file
 BIN_DIR = $(PRO_DIR)/bin
 
 
